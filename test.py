eb10a74e630a2ea398c25866b22196c377ec30c4
Dinkelcrack95 <102606058+Dinkelcrack95@users.noreply.github.com>
2022-08-16 16:59:20 +0200

    write to json

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-# hi, i'm a python file
-print("hi, i'm a python file")
+import json
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+
+# Example
+data = {}
+data['key'] = 'value'
+
+writeToJSONFile('./','file-name',data)
+# './' represents the current directory so the directory save-file.py is in
+# 'test' is my file name
